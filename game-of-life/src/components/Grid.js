00a6643940cbf20e94ee2d44d490fc2238d7c54e
cspraggett <cspraggett@gmail.com>
2020-04-15 20:13:57 -0400

    work on basic grid

---
+++ game-of-life/src/components/Grid.js
@@ -0,0 +1,119 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(10, 2vw);
+  text-align: center;
+`;
+
+const Square = styled.a`
+  border: 1px solid black;
+`;
+
+export default function Grid() {
+  return (
+    <Container>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+      <Square>x</Square>
+    </Container>
+  );
+}
