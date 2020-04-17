60fa7a4550262c2de7a7431fbfbd738ada254340
amandena <manda.panda91@yahoo.com>
2020-04-16 22:11:59 -0400

    set up initial file structure

---
+++ amandas-project/src/containers/Game.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Game extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1>Amanda's Game of Life</h1>
+      </div>
+    )
+  }
+}
+
+export default Game
