eaca817a1cfe95fb774a50ae08841496e2fd420f
amandena <manda.panda91@yahoo.com>
2020-04-19 02:38:59 -0400

    added new constructor and global state, & removed previous class functions

--- amandas-project/src/containers/Game.js
+++ amandas-project/src/containers/Game.js
@@ -5,37 +5,15 @@ const WIDTH = 400
 const HEIGHT = 400
 
 class Game extends React.Component {
-  createRows = rows => { //creates rows
-    let arr = []
-    for(let i = 0; i < rows; i++) {
-      arr[i] = []
-    }
-    return arr
-  }
-  
-  fillRandom = () => { //fills the grid randomly
-    let theGrid = this.createRows(400)
-    for(let j = 0; j < this.state.gridHeight; j++) { //iterate through rows
-      for(let k = 0; k < this.state.gridHeight; k++) { //interate through columns
-        let random = Math.random()
-        let int = (random * 2)
-        let oneOrZero = Math.floor(int)
-        theGrid[j][k] = oneOrZero
-      }
-    }
-    return theGrid
+  constructor() {
+    super()
+    this.rows = HEIGHT / CELL_SIZE
+    this.cols = WIDTH / CELL_SIZE
+    
   }
 
-  drawGrid = () => { // draws grid on canvas 
-    for(let j = 1; j < this.state.gridHeight; j++) { // iterate through rows
-      for(let k = 0; k < this.state.gridWidth; k++) { // iterate through columns
-        if(this.fillRandom()[j][k] === 1) {
-          // console.log(this.props.ctx.getContext('2d').)
-          // ctx.fillStyle = '#FF0000'
-          // ctx.fillRect(j,k,1,1)
-        }
-      }
-    }
+  state = {
+    cells: []
   }
 
   render() {
