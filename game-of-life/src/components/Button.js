aaeb6b86eadc99a654189189120292708679f5cd
cspraggett <cspraggett@gmail.com>
2020-04-19 11:21:31 -0400

    change text when start button is clicked

--- game-of-life/src/components/Button.js
+++ game-of-life/src/components/Button.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 const Buttons = styled.button``;
 
 export default function Button({ grid, nextMove, changeAllMoves }) {
-  let text = "Start";
+  // let text = "Start";
+  const [text, setText] = useState("Start");
+
+  const changeText = () => {
+    setText(text === "Start" ? "Stop" : "Start");
+  };
+
+  const clicked = () => {
+    changeAllMoves();
+    changeText();
+  };
   return (
     <div>
       <Buttons
@@ -13,7 +23,7 @@ export default function Button({ grid, nextMove, changeAllMoves }) {
       >
         next
       </Buttons>
-      <Buttons onClick={() => changeAllMoves()}>{text}</Buttons>
+      <Buttons onClick={() => clicked()}>{text}</Buttons>
     </div>
   );
 }
