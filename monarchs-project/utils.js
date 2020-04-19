af74c986d7d06fbe93a10882d8c70c3b71163bc2
Monarch Wadia <monarch@zeroprojects.ca>
2020-04-19 13:28:28 -0400

    Finished simple node-based conways game of life.

--- monarchs-project/utils.js
+++ monarchs-project/utils.js
@@ -1,12 +1,21 @@
-const { ROW_SIZE, COL_SIZE, ON, OFF } = require('./constants');
+const { ON, INHERIT } = require('./constants');
+const { ConwaysGameEngine } = require('./index');
 
-function times(iterations = 1, callback) {
+function times(iterations, callback) {
   for (let i = 0; i < iterations; i++) {
     callback(i);
   }
 }
 
-function getNumberOfNeighbours(world, homeRow, homeCol) {
+/**
+ * 
+ * @param {ConwaysGameEngine} engine 
+ * @param {*} homeRow 
+ * @param {*} homeCol 
+ */
+function getNumberOfNeighbours(engine, homeRow, homeCol) {
+  const world = engine.world;
+  const { rowSize, colSize } = engine.config;
 
   // scan the neighbourhood
   const startingRow = homeRow - 1;
@@ -19,13 +28,13 @@ function getNumberOfNeighbours(world, homeRow, homeCol) {
 
   for (var currentRow = startingRow; currentRow <= endingRow; currentRow++) {
     // EDGE CASE 1: don't work with out-of-bound rows
-    if (currentRow < 0 || currentRow > (ROW_SIZE - 1)) {
+    if (currentRow < 0 || currentRow > (rowSize - 1)) {
       continue;
     }
 
     for (var currentCol = startingCol; currentCol <= endingCol; currentCol++) {
       // EDGE CASE 2: don't work with out-of-bound cols
-      if (currentCol < 0 || currentCol > (ROW_SIZE - 1)) {
+      if (currentCol < 0 || currentCol > (colSize - 1)) {
         continue;
       }
 
@@ -44,13 +53,13 @@ function getNumberOfNeighbours(world, homeRow, homeCol) {
   return numberOfNeighbours;
 }
 
-function initWorld(valueToSet) {
+function initWorld(valueToSet, rowSize, colSize) {
   const world = [];
 
-  times(ROW_SIZE, row => {
+  times(rowSize, row => {
     world[row] = [];
 
-    times(COL_SIZE, col => {
+    times(colSize, col => {
       world[row][col] = valueToSet;
     })
   });
@@ -62,9 +71,58 @@ function draw(world, row, col) {
   world[row][col] = ON;
 }
 
+/**
+ * 
+ * @param {ConwaysGameEngine} engine 
+ */
+function calculateNewWorldState(engine) {
+  const { world: oldWorld, config } = engine;
+  const { rules, rowSize, colSize, allowMultipleRuleMatches } = config;
+
+  const newWorld = initWorld(INHERIT, rowSize, colSize);
+
+  // process the rules
+  times(engine.config.rowSize, row => {
+    times(engine.config.colSize, col => {
+      const currState = engine.getState(row, col);
+      const numberOfNeighbours = getNumberOfNeighbours(engine, row, col);
+
+      let rulesAlreadyMatched = [];
+      rules.forEach(rule => {
+        const isMatch = rule.matcher(currState, numberOfNeighbours);
+
+        if (isMatch) {
+          rulesAlreadyMatched.push(rule.name);
+
+          // error check
+          if (!allowMultipleRuleMatches && rulesAlreadyMatched.length > 1) {
+            console.error("RULES ALREADY MATCHED", rulesAlreadyMatched)
+            throw new Error('Multiple rulematches detected. This is an illegal state. Game will abort.')
+          }
+
+          newWorld[row][col] = rule.result;
+        }
+      });
+    });
+  });
+
+  // collapse the worlds
+  times(rowSize, row => {
+    times(colSize, col => {
+      if (newWorld[row][col] === INHERIT) {
+        newWorld[row][col] = oldWorld[row][col];
+      }
+    })
+  })
+
+  return newWorld;
+}
+
 module.exports = {
   times,
   getNumberOfNeighbours,
   initWorld,
-  draw
+  draw,
+  calculateNewWorldState
 }
+
